1c7d959975ad11e920dfe961473deb141cc1918b
Anthony Mittaz <sync@me.com>
2019-03-06 21:15:27 +1100

    Simplify code

--- src/match.js
+++ src/match.js
@@ -1,4 +1,4 @@
-import { h, Component, toChildArray } from 'preact';
+import { h, Component } from 'preact';
 import { subscribers, getCurrentUrl, Link as StaticLink } from 'preact-router';
 
 export class Match extends Component {
@@ -16,8 +16,7 @@ export class Match extends Component {
 		let url = this.nextUrl || getCurrentUrl(),
 			path = url.replace(/\?.+$/,'');
 		this.nextUrl = null;
-		const children = toChildArray(props.children);
-		return children[0] && children[0]({
+		return props.children({
 			url,
 			path,
 			matches: path===props.path
