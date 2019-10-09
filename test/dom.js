317a8cdd3aa0302927e65b4a344d832ac6436941
Yashraj Chavda <yashrajpchavda@gmail.com>
2019-10-09 17:30:45 +0530

    nested-routes: updated a test case

--- test/dom.js
+++ test/dom.js
@@ -11,14 +11,11 @@ function fireEvent(on, type) {
 }
 
 describe('dom', () => {
-	let scratch, $, mount, base;
+	let scratch, $, mount;
 
 	before( () => {
 		scratch = document.createElement('div');
 		document.body.appendChild(scratch);
-		base = document.createElement('base');
-		base.setAttribute('href', '/');
-		document.body.appendChild(base);
 		$ = s => scratch.querySelector(s);
 		mount = jsx => render(jsx, scratch, scratch.firstChild);
 	});
